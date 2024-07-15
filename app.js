5354796f3b87784f84eaa2c7ea6e986c12d1b648
tii_ui <suontii2023@gmail.com>
2024-07-15 17:10:41 +0700

    Design page contact us

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Define a route for the home page
 app.get('/', (req, res) => {
-    res.render('index.view.ejs', { title: 'Home Page' });
+    const currentRoute = req.path;
+    res.render('index.view.ejs', { title: 'Home Page', currentRoute: '/' });
+});
+
+app.get('/shop', (req, res) => {
+    res.render('shop.view.ejs', { title: 'Shop', currentRoute: '/shop' });
 });
 
 app.listen(PORT, () => {
