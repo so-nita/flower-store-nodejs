a1f1a10387dce0ac43a91021133ce78151eaa6af
tii_ui <suontii2023@gmail.com>
2024-07-16 15:05:35 +0700

    Integrate navbar not success

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -9,12 +9,25 @@ app.set('view engine', 'ejs');
 
 // Set the directory for views
 app.set('views', path.join(__dirname, 'views'));
+app.set('view options', { debug: true });
 
 // Define a route for the home page
-app.get('/', (req, res) => {
-    const currentRoute = req.path;
-    res.render('index.view.ejs', { title: 'Home Page', currentRoute: '/' });
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://so-nita.github.io/mock-api-flowerstore/category.json');
+        const navlinks = response.data;
+
+        res.render('index.view.ejs', {
+            title: 'Home Page',
+            currentRoute: '/',
+            navlinks
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
 
 app.get('/shop', (req, res) => {
     res.render('shop.view.ejs', { title: 'Shop', currentRoute: '/shop' });
