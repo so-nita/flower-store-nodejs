17b90e46abfc2cb188f96e0e3418d8c88a9e9ad3
TiiTuy2 <suonty2023@gmail.com>
2024-07-16 21:02:27 +0700

    Integrate navbar

--- app.js
+++ app.js
@@ -10,26 +10,43 @@ app.set('view engine', 'ejs');
 // Set the directory for views
 app.set('views', path.join(__dirname, 'views'));
 
+// Function to fetch navigation links
+async function fetchNavLinks() {
+    try {
+        const response = await axios.get('https://so-nita.github.io/mock-api-flowerstore/category.json');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw new Error('Failed to fetch navigation links');
+    }
+}
+
 // Define a route for the home page
 app.get('/', async (req, res) => {
     try {
-        const response = await axios.get('https://so-nita.github.io/mock-api-flowerstore/category.json');
-        const navlinks = response.data;
-
+        const navlinks = await fetchNavLinks();
         res.render('index.view.ejs', {
             title: 'Home Page',
             currentRoute: '/',
             navlinks: navlinks
         });
     } catch (error) {
-        console.error('Error fetching data:', error);
         res.status(500).send('Internal Server Error');
     }
 });
 
-
-app.get('/shop', (req, res) => {
-    res.render('shop.view.ejs', { title: 'Shop', currentRoute: '/shop' });
+// Define a route for the shop page
+app.get('/shop', async (req, res) => {
+    try {
+        const navlinks = await fetchNavLinks();
+        res.render('shop.view.ejs', {
+            title: 'Shop',
+            currentRoute: '/shop',
+            navlinks: navlinks
+        });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(PORT, () => {
